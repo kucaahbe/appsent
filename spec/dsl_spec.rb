de502dd7e412be5a5f149c7b940856afa17269bc
kucaahbe <kucaahbe@ukr.net>
2011-03-31 14:11:24 +0300

    some spec update

--- spec/dsl_spec.rb
+++ spec/dsl_spec.rb
@@ -52,8 +52,52 @@ describe "AppSent.init" do
 
     subject { described_class }
 
-    it "should raise exception if unsupported type passed" do
-      expect { subject.new(:some_wrong_type) }.to raise_exception(/unsupported data type: :some_wrong_type\. Data types you should use: \[ (.*,?)+ \]/)
+    before :each do
+      @good_params = [ String, 'some string', 'description string', 'example string' ]
+    end
+
+    context ".new" do
+
+      it "should raise exception if unsupported type passed" do
+	expect { subject.new('asd','data') }.to raise_exception(/:type should be ruby class!/)
+      end
+
+    end
+
+    context "#valid?" do
+
+      context "should return false" do
+
+	it "if entry does not presence in config file" do
+	  subject.new(String,nil).should_not be_valid
+	end
+
+	it "if data in config file has wrong type" do
+	  subject.new(String,2).should_not be_valid
+	end
+
+      end
+
+      context "should return true" do
+
+	it "if entry presence and has right type" do
+	  subject.new(*@good_params).should be_valid
+	end
+
+      end
+
+    end
+
+    context "#error_message" do
+
+      it "should be nil if valid" do
+	pending
+      end
+
+      it "should generate correct error message if invalid" do
+	pending
+      end
+
     end
   end
 
