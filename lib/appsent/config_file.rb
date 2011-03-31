2fcfd5cac049f390601dabe7af1b8848e929374f
kucaahbe <kucaahbe@ukr.net>
2011-03-31 18:35:59 +0300

    ConfigFile

--- lib/appsent/config_file.rb
+++ lib/appsent/config_file.rb
@@ -1,4 +1,19 @@
 class AppSent
-  class ConfigFile < ConfigValue
+  class ConfigFile
+    attr_reader :data
+
+    def initialize config_dir, config_file_name, environment, type=Hash
+      @config_dir, @config_file_name, @environment, @type = config_dir, config_file_name, environment.to_s, type
+    end
+
+    def valid?
+      yaml_data = YAML.load_file(File.join(@config_dir,@config_file_name))
+      yaml_data.keys.each { |key| yaml_data[key.to_s] = yaml_data.delete(key) }
+
+      @data = yaml_data[@environment]
+      @data.instance_of?(@type)
+    rescue Errno::ENOENT
+      false
+    end
   end
 end
--- spec/internal_api/config_file_spec.rb
+++ spec/internal_api/config_file_spec.rb
@@ -5,9 +5,46 @@ describe AppSent::ConfigFile do
   subject { described_class }
 
   before :each do
-    @params = ['config_name']
+    @params = ['/path/to/config','config_name',:environment]
   end
 
-  it ""
+  context "#valid?" do
+
+    context "should be true" do
+
+      it "if config exists and environment presence(without type)" do
+	YAML.should_receive(:load_file).once.with('/path/to/config/config_name').and_return('environment' => {:a=>100500})
+	subject.new(*@params).should be_valid
+      end
+
+      it "if config exists and environment presence(with type specified)" do
+	YAML.should_receive(:load_file).once.with('/path/to/config/config_name').and_return(:environment => [1,2,3])
+	subject.new(*@params,Array).should be_valid
+      end
+
+    end
+
+    context "should be false" do
+
+      it "if config does not exists" do
+	YAML.should_receive(:load_file).once.with('/path/to/config/config_name').and_raise(Errno::ENOENT)
+      end
+
+      it "if environment does not presence in config" do
+	YAML.should_receive(:load_file).once.with('/path/to/config/config_name').and_return('other_environment' => {:a=>100500})
+      end
+
+      it "if wrong type" do
+	@params << Array
+	YAML.should_receive(:load_file).once.with('/path/to/config/config_name').and_return(:environment => 123)
+      end
+
+      after :each do
+	subject.new(*@params).should_not be_valid
+      end
+
+    end
+
+  end
 
 end
