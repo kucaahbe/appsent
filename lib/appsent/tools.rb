84c0c88e5cebf21c040e6f3c1c2d54e623c8b466
kucaahbe <kucaahbe@ukr.net>
2011-07-29 01:40:02 +0300

    this should be included...

--- lib/appsent/tools.rb
+++ lib/appsent/tools.rb
@@ -1,5 +1,6 @@
 class Hash
-  unless self.respond_to?(:symbolize_keys!)
+  # fix  bug here
+  unless self.method_defined?(:symbolize_keys!)
     def symbolize_keys!
       self.keys.each { |key| self[(key.to_sym rescue key) || key] = self.delete(key) }
     end
