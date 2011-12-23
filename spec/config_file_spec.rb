08abb02bfec3baef31ff76a15dd48fd4e9e768db
Dmitry Mjakotny <kucaahbe@ukr.net>
2011-12-23 12:58:32 +0200

    refactored config_file_spec.rb

--- spec/config_file_spec.rb
+++ spec/config_file_spec.rb
@@ -105,25 +105,29 @@ describe AppSent::ConfigFile do
 
   context "#valid?" do
 
+    def yaml_loads_file_and_return(*something)
+      YAML.should_receive(:load_file).once.with(fake_config_filename).and_return(*something)
+    end
+
     context "should be true" do
 
       it "if config exists and environment presence(without type)(no values)" do
-	YAML.should_receive(:load_file).once.with(fake_config_filename).and_return('environment' => {:a=>100500})
-	subject.new(*params).should be_valid
+        yaml_loads_file_and_return('environment' => {:a=>100500})
+        subject.new(*params).should be_valid
       end
 
       it "if config exists and environment presence(with type specified)(no values)" do
-	@params['type']=Array
-	YAML.should_receive(:load_file).once.with(fake_config_filename).and_return(:environment => [1,2,3])
-	subject.new(*params).should be_valid
+        @params['type']=Array
+        yaml_loads_file_and_return(:environment => [1,2,3])
+        subject.new(*params).should be_valid
       end
 
       it "if config exists and environment presence(with values)" do
-	values_block = lambda {
-	  value String
-	}
-	YAML.should_receive(:load_file).once.with(fake_config_filename).and_return('environment' => {:value=>'100500'})
-	subject.new(*params,&values_block).should be_valid
+        values_block = lambda {
+          value String
+        }
+        yaml_loads_file_and_return('environment' => {:value=>'100500'})
+        subject.new(*params,&values_block).should be_valid
       end
 
     end
@@ -131,20 +135,20 @@ describe AppSent::ConfigFile do
     context "should be false" do
 
       it "if config does not exists" do
-	YAML.should_receive(:load_file).once.with(fake_config_filename).and_raise(Errno::ENOENT)
+        YAML.should_receive(:load_file).once.with(fake_config_filename).and_raise(Errno::ENOENT)
       end
 
       it "if environment does not presence in config" do
-	YAML.should_receive(:load_file).once.with(fake_config_filename).and_return('other_environment' => {:a=>100500})
+        yaml_loads_file_and_return('other_environment' => {:a=>100500})
       end
 
       it "if wrong type" do
-	@params['type'] = Array
-	YAML.should_receive(:load_file).once.with(fake_config_filename).and_return(:environment => 123)
+        @params['type'] = Array
+        yaml_loads_file_and_return(:environment => 123)
       end
 
       after :each do
-	subject.new(*params).should_not be_valid
+        subject.new(*params).should_not be_valid
       end
 
     end
